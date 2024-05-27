94c5a5c52adef9e13e9c3a3dff58d70639b72841
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-05-27 17:50:24 -0500

    Corrigiendo error en la imagen svg

--- app/oficio/page.js
+++ app/oficio/page.js
@@ -113,7 +113,7 @@ function Oficio() {
   const [email, guardaEmail] = useState("");
 
   const [showTooltip, setShowTooltip] = useState(false);
-  const screenWidth = window.innerWidth;
+  const screenWidth = typeof window !== "undefined" ? window.innerWidth : 768;
 
   const handleChangeNombre = (e) => {
     e.preventDefault();
@@ -359,16 +359,16 @@ function Oficio() {
                         width="18"
                         height="18"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        strokeWidth="1.5"
                         stroke="#2c3e50"
                         fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       >
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path
                           d="M10.425 1.414a3.33 3.33 0 0 1 3.026 -.097l.19 .097l6.775 3.995l.096 .063l.092 .077l.107 .075a3.224 3.224 0 0 1 1.266 2.188l.018 .202l.005 .204v7.284c0 1.106 -.57 2.129 -1.454 2.693l-.17 .1l-6.803 4.302c-.918 .504 -2.019 .535 -3.004 .068l-.196 -.1l-6.695 -4.237a3.225 3.225 0 0 1 -1.671 -2.619l-.007 -.207v-7.285c0 -1.106 .57 -2.128 1.476 -2.705l6.95 -4.098zm1.575 9.586h-1l-.117 .007a1 1 0 0 0 0 1.986l.117 .007v3l.007 .117a1 1 0 0 0 .876 .876l.117 .007h1l.117 -.007a1 1 0 0 0 .876 -.876l.007 -.117l-.007 -.117a1 1 0 0 0 -.764 -.857l-.112 -.02l-.117 -.006v-3l-.007 -.117a1 1 0 0 0 -.876 -.876l-.117 -.007zm.01 -3l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007z"
-                          stroke-width="0"
+                          strokeWidth="0"
                           fill="currentColor"
                         />
                       </svg>
@@ -379,12 +379,19 @@ function Oficio() {
                       className="absolute bg-gray-800 text-white py-2 px-3 rounded text-sm z-10 pointer-events-none"
                       style={{
                         left: "50%",
-                        transform: screenWidth<=768 ? "translateX(-50%)" : "translateX(-11%)",
+                        transform:
+                          screenWidth <= 768
+                            ? "translateX(-50%)"
+                            : "translateX(-11%)",
                         marginTop: "0.5rem",
-                        width: "345px"
+                        width: "345px",
                       }}
                     >
-                      El período al que aplica la actividad, NO es necesariamente el período de estudio que está cursando porque dependerá si está arrastrando, si está al día o si está adelantando la actividad de vinculación. En caso de dudas comunicarse con el Responsable de Vinculación.
+                      El período al que aplica la actividad, NO es
+                      necesariamente el período de estudio que está cursando
+                      porque dependerá si está arrastrando, si está al día o si
+                      está adelantando la actividad de vinculación. En caso de
+                      dudas comunicarse con el Responsable de Vinculación.
                     </div>
                   )}
                   <select
