5b5bda70097ddf79ddab85beb9ed4251c34789e0
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-06-06 20:09:33 -0500

    Permitiendo temporalmente el acceso a usuarios no registrados

--- components/ui/Oficio.jsx
+++ components/ui/Oficio.jsx
@@ -293,7 +293,7 @@ function OficioComponent() {
   }
 
   return ( !loadPage &&
-         (auth?.nombre ?
+         //(auth?.nombre ?
       <div className="flex flex-col">
         <h1 className="uppercase text-2xl font-bold mb-10 flex justify-center items-center">
           Solicitud de número de Oficio
@@ -315,13 +315,13 @@ function OficioComponent() {
                   <input
                     id="name"
                     type="text"
-                    className="mt-2 text-gray-800 block w-full p-3 bg-gray-200 hover:cursor-not-allowed"
+                    className="mt-2 text-gray-800 block w-full p-3 bg-gray-50"// hover:cursor-not-allowed"
                     placeholder="Coloque un nombre y un apellido"
                     name="name"
                     value={nombre}
                     onChange={handleChangeNombre}
                     maxLength={30}
-                    disabled
+                    //disabled
                   />
                   <p className="text-red-500">{aviso}</p>
                 </div>
@@ -660,14 +660,15 @@ function OficioComponent() {
           )
         )}
       </div>
-        :
-        <div className="flex flex-col">
-        <h1 className="uppercase text-2xl font-bold mb-10 flex justify-center items-center">
-          Solicitud de número de Oficio
-        </h1>
-        <p className="flex justify-center uppercase font-bold">Debe iniciar sesión para acceder a esta aplicación</p>
-        </div>
-  ));
+      //   :
+      //   <div className="flex flex-col">
+      //   <h1 className="uppercase text-2xl font-bold mb-10 flex justify-center items-center">
+      //     Solicitud de número de Oficio
+      //   </h1>
+      //   <p className="flex justify-center uppercase font-bold">Debe iniciar sesión para acceder a esta aplicación</p>
+      //   </div>
+      // )
+);
 }
 
 export default OficioComponent;
