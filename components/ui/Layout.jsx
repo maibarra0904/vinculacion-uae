8e674e06aca38ff1b8cf3f3ee5f8510551cc4bab
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-09-06 00:49:11 -0500

    Haciendo un ajuste al modal para que se vea solo una vez

--- components/ui/Layout.jsx
+++ components/ui/Layout.jsx
@@ -14,39 +14,48 @@ const Layout = ({ children }) => {
 
     const [confirmed, setConfirmed] = useState(false)
 
+    const [viewModal, setViewModal] = useState(true)
+
     const [loadPage, setLoadPage] = useState(true)
 
     const [isModalOpen, setIsModalOpen] = useState(true);
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+    //const openModal = () => setIsModalOpen(true);
+    const closeModal = () => {
+        setIsModalOpen(false)
+        localStorage.setItem('view', JSON.stringify(true))
+    }
 
     useEffect(() => {
-      
+
         setAuth(isClient ? JSON.parse(localStorage.getItem("usuario")) || {} : {})
         setConfirmed(isClient ? JSON.parse(localStorage.getItem("confirmed")) || false : false)
+        setViewModal(isClient ? JSON.parse(localStorage.getItem("view")) || false : false)
         setLoadPage(false)
 
-    },[])
+    }, [])
 
     return (
         <MyContext.Provider value={{ auth, setAuth, confirmed, setConfirmed, loadPage, setLoadPage }}>
-        <div className="md:flex">
-            
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-            
-        <h1 className="flex justify-center mt-2 mb-4 uppercase">Atención!</h1>
-        <h2>La emisión de números de oficios quedarán inhabilitados temporalmente desde el <span className="font-bold">07/09/2024</span> hasta el <span className="font-bold">16/09/2024</span></h2>
-        
-      </Modal>
-            
-            <OrderSidebar />
-            
-            
-            <div className="p-5 md:flex-1 md:h-screen md:overflow-y-scroll">
-                {children}
+            <div className="md:flex">
+                {
+                    !viewModal &&
+                    <Modal isOpen={isModalOpen} onClose={closeModal}>
+
+                        <h1 className="flex justify-center mt-2 mb-4 uppercase">Atención!</h1>
+                        <h2>La emisión de números de oficios quedarán inhabilitados temporalmente desde el <span className="font-bold">07/09/2024</span> hasta el <span className="font-bold">16/09/2024</span></h2>
+
+                    </Modal>
+                }
+
+
+                <OrderSidebar />
+
+
+                <div className="p-5 md:flex-1 md:h-screen md:overflow-y-scroll">
+                    {children}
+                </div>
             </div>
-        </div>
         </MyContext.Provider>
     )
 }
