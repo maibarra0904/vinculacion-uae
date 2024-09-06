26b1c185c6530a1ee8695eb1ed8b9d17f0ca2ffa
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-09-06 00:33:05 -0500

    Agregando modal para advertencia de suspención de emisión de oficios

--- components/ui/Layout.jsx
+++ components/ui/Layout.jsx
@@ -2,11 +2,13 @@
 import MyContext from "../context/myContext"
 import OrderSidebar from "../order/OrderSidebar"
 import { useEffect, useState } from "react"
+import Modal from "./Modal"
 
 
 const Layout = ({ children }) => {
 
     const isClient = typeof window !== 'undefined';
+
 
     const [auth, setAuth] = useState({})
 
@@ -14,16 +16,29 @@ const Layout = ({ children }) => {
 
     const [loadPage, setLoadPage] = useState(true)
 
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
     useEffect(() => {
+      
         setAuth(isClient ? JSON.parse(localStorage.getItem("usuario")) || {} : {})
         setConfirmed(isClient ? JSON.parse(localStorage.getItem("confirmed")) || false : false)
         setLoadPage(false)
+
     },[])
 
     return (
         <MyContext.Provider value={{ auth, setAuth, confirmed, setConfirmed, loadPage, setLoadPage }}>
         <div className="md:flex">
             
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+            
+        <h1 className="flex justify-center mt-2 mb-4 uppercase">Atención!</h1>
+        <h2>La emisión de números de oficios quedarán inhabilitados temporalmente desde el <span className="font-bold">07/09/2024</span> hasta el <span className="font-bold">16/09/2024</span></h2>
+        
+      </Modal>
             
             <OrderSidebar />
             
---
+++ components/ui/Modal.jsx
@@ -0,0 +1,38 @@
+// components/ModalComponent.jsx
+import React from 'react';
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content m-20">
+        <div className='flex justify-end'>
+        <button onClick={onClose} className='px-3 py-1 bg-red-400 text-white rounded-lg'>x</button>
+        </div>
+        {children}
+      </div>
+      <style jsx>{`
+        .modal-overlay {
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background: rgba(0, 0, 0, 0.5);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .modal-content {
+          background: white;
+          padding: 20px;
+          border-radius: 5px;
+          box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Modal;
