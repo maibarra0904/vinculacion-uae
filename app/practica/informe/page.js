9c356075fdfd09e84252bff0b835120cf3f0123b
Mario Ibarra <mario.ibarra.86@gmail.com>
2025-03-13 22:39:53 -0500

    Actualizacion de procedimiento de PPP

--- app/practica/informe/page.js
+++ app/practica/informe/page.js
@@ -17,7 +17,7 @@ const Final = () => {
           url={
             "https://uagrariaec-my.sharepoint.com/:f:/g/personal/mibarra_uagraria_edu_ec/EpPp-2chLy9OkJPXOH573e4B1ZFW0lg7WV_Y0fcb8aiGeA?e=j6vbGm"
           }
-          text="Documentación Habilitante para el Cierre de PP"
+          text="DOCUMENTACIÓN habilitante para el Cierre de PP"
           nuevo={true}
           external={true}
         />
@@ -25,13 +25,13 @@ const Final = () => {
               url={
                 "/oficio"
               }
-              text="Aplicación para solicitar número de oficio"
+              text="APLICACIÓN para solicitar número de oficio"
             />
       </EnlacesRelacionados>
       <EnlacesRelacionados text="Otros enlaces de Interés">
         <Enlace
-          url={"https://uagrariaec-my.sharepoint.com/:b:/g/personal/mibarra_uagraria_edu_ec/EfvsHKUVh9tLneCfApkqRekBbRrBo6rNQbxbHu1AYaC78A?e=0DYtNS"}
-          text="Ejemplo de documentos de cierre de PP con recibido"
+          url={"https://uagrariaec-my.sharepoint.com/:b:/g/personal/mibarra_uagraria_edu_ec/EZB6wtSbBT5MvuYWZbaiacUBMfSwqYQuW8pRJezwLFBqIA"}
+          text="Ejemplo de informe aprobado"
           nuevo={true}
           external={true}
         />
