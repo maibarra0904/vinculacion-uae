fce935f4b90e174c4cfa3f1c58af1f7c5dddc2af
Mario Ibarra <mario.ibarra.86@gmail.com>
2026-03-22 13:50:50 -0500

    Actualizando nuevamente

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'], // Removed md and mdx
   reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 }
 
 export default nextConfig;
